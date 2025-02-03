b26a9ce1e3123d2494486726c1baa0a0508b8d20
Patrick Bell <patrick@Patricks-Laptop.local>
2025-02-03 15:45:44 +0000

    update

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { ThemeProviderWrapper } from './components/context/ThemeContext';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
     <ThemeProviderWrapper>
     <App />
     </ThemeProviderWrapper>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
