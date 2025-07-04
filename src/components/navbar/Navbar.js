f190809f445fd6da6241414e4eba0caec8b1c96d
Patrick-Bell <144367992+Patrick-Bell@users.noreply.github.com>
2025-07-04 23:56:44 +0100

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -4,7 +4,7 @@ import Logo from '../assets/qr-logo.png'
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import QrCodeScannerIcon from '@mui/icons-material/QrCodeScanner';
 import { useNavigate } from "react-router-dom";
-
+ 
       const Navbar = () => {
   const [anchorEl, setAnchorEl] = useState(null);
   const open = Boolean(anchorEl);
