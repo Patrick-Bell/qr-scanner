5cc2ea6220616f790c4f19a7629962f40e285499
Patrick-Bell <144367992+Patrick-Bell@users.noreply.github.com>
2025-08-19 23:47:43 +0100

    Update AppContent.js

--- src/components/app/AppContent.js
+++ src/components/app/AppContent.js
@@ -1,6 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
  import Home from '../pages/Home'
- import QrPage from '../pages/QrPage'
+  import QrPage from '../pages/QrPage'
 
 const AppContent = () => {
 
